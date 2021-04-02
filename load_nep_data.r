e76e56fbb2675eea08a2445c41925cdeb6cc8d3f
Mitchell Mares <Mitchell@vpn1722513670.its.yale.internal>
2021-04-01 21:59:54 -0500

    Added 2000 and 2002 data

--- load_nep_data.r
+++ load_nep_data.r
@@ -9,7 +9,8 @@ pacman::p_load(haven)
 ##########################################
 ##  Load National Exit Poll Data Files  ##
 ##########################################
-
+nep2000 <- read_por("./data/2000_nep.por")
+nep2002 <- read_por("./data/2002_nep.por")
 nep2004 <- read_por("./data/2004_nep.por")
 nep2006 <- read_por("./data/2006_nep.por")
 nep2008 <- read_por("./data/2008_nep.por")
@@ -18,3 +19,4 @@ nep2012 <- read_por("./data/2012_nep.por")
 nep2014 <- read_por("./data/2014_nep.por")
 nep2016 <- read_por("./data/2016_nep.por")
 nep2018 <- read_por("./data/2018_nep.por")
+
